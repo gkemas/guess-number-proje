2bca41cb0b6ffee1f1fb9a898f5aaed03e48511f
gokhan <kemasgokhan@gmail.com>
2022-06-04 23:13:31 +0300

    a

--- app.js
+++ app.js
@@ -4,55 +4,32 @@ const tahmin = document.getElementById("tahmin");
 const girilen = document.getElementById("input");
 const buton = document.getElementById("buton");
 
+let arr = [];
+let arr2 = [];
+let fark1 = [];
+let fark2 = [];
 buton.addEventListener("click", function () {
   bul(girilen.value);
 });
-let arr = [];
 function bul() {
   if (girilen.value == sayı) {
     tahmin.innerText = `Tebrikler sayı ${girilen.value}`;
-  } else {
-    if (girilen.value > sayı) {
-      arr.push(girilen.value);
-      let fark1 =[];
-  for(let i=1; i< length.arr-1; i++ ){
-    fark1.push(arr[i]-sayı);
-    let a =Math.min(fark1);
-    let b =Math.min(fark2);
-    // tahmin.innerText =`${fark1} -${fark2} Arasında Giriniz.`;
-  
-  }
-
-      console.log(arr[0]);
-      // ipucu();
-    }
-  }
-else {
-  let fark2 =[];
-  for(let i=1; i< length.arr-1; i++ ){
-    fark2.push(sayı-arr[i]);
-    let a =Math.min(fark1);
-  let b =Math.min(fark2);
-  // tahmin.innerText =`${fark1} -${fark2} Arasında Giriniz.`
     
+  } else if (girilen.value > sayı) {
+    arr.push(girilen.value);
+    fark1 = arr.sort();
+    tahmin.innerText = `${fark2[fark2.length - 1]} -${
+      fark1[0]
+    } Arasında Giriniz.`;
+  } else if (girilen.value < sayı) {
+    arr2.push(girilen.value);
+    fark2 = arr2.sort();
+    tahmin.innerText = `${fark2[fark2.length - 1]} -${
+      fark1[0]
+    } Arasında Giriniz.`;
   }
-
-
-}
-}
-
-function ipucu(){
-  let fark1 =[];
-  for(let i=1; i< length.arr-1; i++ ){
-    fark1.push(arr[i]-sayı);
-  }
-  let fark2 =[];
-  for(let i=1; i< length.arr-1; i++ ){
-    fark1.push(sayı-arr[i]);
     
-  }
-  let a =Math.min(fark1);
-  let b =Math.min(fark2);
-  tahmin.innerText =`${fark1} -${fark2} Arasında Giriniz.`
-
+  // tahmin.innerText = `${fark2[fark2.length - 1]} -${
+  //   fark1[0]
+  // } Arasında Giriniz.`;
 }
